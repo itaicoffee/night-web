a48f413baa67b70df526ddfbc46272bad9b1a0cc
itai <idh1729@gmail.com>
2023-01-01 17:23:10 -0500

    Add user login

---
+++ src/Interfaces.tsx
@@ -0,0 +1,16 @@
+
+export interface Entry {
+  uid: string;
+  userUid: string;
+  day: string;
+  fromTime: string;
+  toTime: string;
+  numSeats: number;
+  venueNames: string[];
+}
+
+export interface User {
+  uid: string;
+  username: string;
+  token: string;
+}
